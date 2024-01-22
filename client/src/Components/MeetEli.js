0e2cd7e3faf1ddafb18f97277e659a8b0190b1bc
Muhammad Usama <usamabajwa3030@gmail.com>
2024-01-22 13:23:26 +0500

    update meeteli.js

--- client/src/Components/MeetEli.js
+++ client/src/Components/MeetEli.js
@@ -14,26 +14,6 @@ function MeetEli() {
       id="MEET_ELI"
       className="min-h-auto relative m-0 bg-white flex flex-col-reverse md:flex-row"
     >
-      {/* <div className="left w-full md:w-1/2 mt-20 mb-8 md:mt-0 md:mb-0 relative flex items-center justify-center z-2">
-        <img
-          src={Tomato}
-          alt="Bg-Tomato"
-          className="h-40 md:h-56 w-40 md:w-52 object-cover absolute top-0 right-12 md:right-24 z-0"
-        />
-        <img
-          src={MeetEliMain}
-          alt="Main ELI"
-          className="h-60 md:h-80 w-auto absolute mt-16 md:mt-28 z-2 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-        />
-      </div> */}
-      <div className="left w-full md:w-1/2 mt-0 mr-0 md:mb-0 flex items-center justify-center z-2">
-        <img
-          src={GroupImage}
-          alt="Main ELI"
-          className="h-80 w-auto lg:h-[22rem] lg:w-[27rem]"
-        />
-      </div>
-
       <div className="right z-2 w-full md:w-1/2 z-2 mt-8 md:mt-10 mr-0 lg:mb-8 mb-0 flex flex-col justify-center p-4">
         <div className=" mr-0 ml-[12%] lg:ml-[1%]" style={{ marginTop: "5%" }}>
           <h2 className="text-6xl text-left font-extrabold font-[FatFrank] text-[#3C7278] mb-4">
