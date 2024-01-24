5ac7eb3f5942cd2330a7eb84de44dab5752267c9
Muhammad Usama <usamabajwa3030@gmail.com>
2024-01-24 14:50:23 +0500

    chatbot added...

--- client/src/Components/AskMyCow.js
+++ client/src/Components/AskMyCow.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import AI_cow from "../assets/AI_Cow.png";
 
 function AskMyCow() {
-  const text = "The answer to the question will be displayed here.";
+  const [text, setText] = useState(
+    "The answer to the question will be displayed here."
+  );
+  const [question, setQuestion] = useState("");
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://chatling.ai/js/embed.js";
+    script.async = true;
+    script.setAttribute("data-id", "1182799319");
+    script.id = "chatling-embed-script";
+    script.onload = () => {
+      window.Chatling &&
+        window.Chatling.on("ready", function () {
+          console.log("Chatling is ready");
+        });
+    };
+    document.body.appendChild(script);
+  }, []);
+
+  const openChat = () => {
+    const chatling = window.Chatling;
+    if (chatling) {
+      chatling.open();
+    } else {
+      console.log("Chatling is not ready yet, trying again in 1 second");
+      setTimeout(openChat, 1000);
+    }
+  };
   return (
     <div
       id="ASK_MY_COW"
@@ -27,10 +55,13 @@ function AskMyCow() {
           id="question"
           type="text"
           placeholder="Ask a question"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
         />
         <button
           id="Question"
           className="bg-white hover:bg-gray-100 block text-[#3C7278] py-1 px-4 mb-4 mt-2 rounded-lg"
+          onClick={openChat}
         >
           ASK MYRTLE
         </button>
@@ -52,23 +83,22 @@ function AskMyCow() {
 
 export default AskMyCow;
 
+// simple code without api
 // import React, { useState } from "react";
 // import "./style.css";
 // import AI_cow from "../assets/AI_Cow.png";
 
 // function AskMyCow() {
-//   const [showElements, setShowElements] = useState(window.innerWidth > 768);
+//   const [text, setText] = useState(
+//     "The answer to the question will be displayed here."
+//   );
+//   const [question, setQuestion] = useState("");
 
-//   window.addEventListener("resize", () => {
-//     setShowElements(window.innerWidth > 768);
-//   });
-
-//   const text = "The answer to the question will be displayed here.";
-
+//   const askMyrtle = async () => {};
 //   return (
 //     <div
 //       id="ASK_MY_COW"
-//       className="min-h-auto mt-4 mb-12 text-white bg-gradient-to-r from-greenSlack-100 to-greenSlack-600 flex flex-col md:flex-row"
+//       className="min-h-auto mt-4 mb-0 text-white bg-gradient-to-r from-greenSlack-100 to-greenSlack-600 flex flex-col md:flex-row"
 //     >
 //       <div className="left w-full md:w-1/2 flex flex-col items-center p-8">
 //         <h2 className="text-5xl lg:text-6xl text-center font-extrabold font-[FatFrank] mb-4">
@@ -80,35 +110,27 @@ export default AskMyCow;
 //           plans, and anything else you want to know about choosing and
 //           nutritious plant-based foods and diets.
 //         </p>
-//         {showElements ? (
-//           <>
-//             <p className="text-md text-center font-semibold mb-8">
-//               Ask Myrtle a question below!
-//             </p>
-//             <input
-//               className="appearance-none block w-3/4 bg-transparent border text-white border-white focus:border-white focus:ring-white placeholder-white rounded-xl py-3 px-4 mb-3 leading-tight focus:bg-transparent focus:outline-none"
-//               id="question"
-//               type="text"
-//               placeholder="Ask a question"
-//             />
-//             <button
-//               id="Question"
-//               className="bg-white hover:bg-gray-100 block text-[#3C7278] py-1 px-4 mb-4 mt-2 rounded-lg"
-//             >
-//               ASK MYRTLE
-//             </button>
-//             <div className="w-full h-36 mt-4 mb-8 text-sm p-4 text-white bg-[#26828F] rounded-3xl">
-//               {text}
-//             </div>
-//           </>
-//         ) : (
-//           <button
-//             className="bg-white hover:bg-gray-100 block text-[#3C7278] py-1 px-4 mb-4 mt-2 rounded-lg"
-//             onClick={() => setShowElements(true)}
-//           >
-//             Show me
-//           </button>
-//         )}
+//         <p className="text-md text-center font-semibold mb-8">
+//           Ask Myrtle a question below!
+//         </p>
+//         <input
+//           className="appearance-none block w-3/4 bg-transparent border text-white border-white focus:border-white focus:ring-white placeholder-white rounded-xl py-3 px-4 mb-3 leading-tight focus:bg-transparent focus:outline-none"
+//           id="question"
+//           type="text"
+//           placeholder="Ask a question"
+//           value={question}
+//           onChange={(e) => setQuestion(e.target.value)}
+//         />
+//         <button
+//           id="Question"
+//           className="bg-white hover:bg-gray-100 block text-[#3C7278] py-1 px-4 mb-4 mt-2 rounded-lg"
+//           onClick={askMyrtle}
+//         >
+//           ASK MYRTLE
+//         </button>
+//         <div className="w-full h-36 mt-4 mb-8 text-sm p-4 text-white bg-[#26828F] rounded-3xl">
+//           {text}
+//         </div>
 //       </div>
 
 //       <div className="right w-full md:w-1/2 flex items-center justify-center lg:justify-start">
--- client/src/Components/BookACall.js
+++ client/src/Components/BookACall.js
@@ -2,6 +2,7 @@
 // import "./style.css";
 
 // function BookACall() {
+//   // `${process.env.REACT_APP_NODE_API}/api/signup`
 //   return (
 //     <div id="BOOK_A_CALL" class=" min-h-auto mb-0 w-full bg-[#F4F4F4]">
 //       <div className="BookCall text-center w-5/6 lg:w-3/5 mx-auto">
@@ -18,19 +19,125 @@
 //           book a motivational speaking engagement with Eli.
 //         </p>
 //       </div>
-//       <div className="form w-full "></div>
+//       <form class="w-full max-w-lg mt-12">
+//         <div class="flex flex-wrap -mx-3 mb-6">
+//           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+//             <p class="text-red-500 text-xs">Form Field Required</p>
+//             <input
+//               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+//               id="grid-first-name"
+//               type="text"
+//               placeholder="Name"
+//             />
+//           </div>
+//           <div class="w-full md:w-1/2 px-3 mb-6">
+//             <input
+//               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+//               id="grid-last-name"
+//               type="text"
+//               placeholder="Organization"
+//             />
+//           </div>
+//           <div class="w-full md:w-1/2 px-3 mb-6">
+//             <input
+//               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+//               id="grid-last-name"
+//               type="text"
+//               placeholder="Title/Role"
+//             />
+//           </div>
+//           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+//             <p class="text-red-500 text-xs">Form Field Required</p>
+//             <input
+//               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+//               id="grid-first-name"
+//               type="text"
+//               placeholder="Email"
+//             />
+//           </div>
+//         </div>
+//         <p>This cow's udderly excited to work with you!</p>
+//         <button
+//           id="Question"
+//           type="submit"
+//           className="bg-gradient-to-r from-greenSlack-100 to-greenSlack-600 hover:bg-gradient-to-r hover:from-greenSlack-200 hover:to-greenSlack-400 block text-white font-mono py-1 px-4 mb-4 mt-2 rounded-lg"
+//         >
+//           Submit
+//         </button>
+//       </form>
 //     </div>
 //   );
 // }
 
 // export default BookACall;
 
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./style.css";
 
 function BookACall() {
+  const [formData, setFormData] = useState({
+    name: "",
+    organization: "",
+    title: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const validateForm = () => {
+    let formErrors = {};
+    let formIsValid = true;
+
+    if (!formData["name"]) {
+      formIsValid = false;
+      formErrors["name"] = "*Name is required.";
+    }
+
+    if (!formData["email"]) {
+      formIsValid = false;
+      formErrors["email"] = "*Email is required.";
+    }
+
+    if (typeof formData["email"] !== "undefined") {
+      var pattern = new RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
+      if (!pattern.test(formData["email"])) {
+        formIsValid = false;
+        formErrors["email"] = "*Please enter valid email.";
+      }
+    }
+
+    setErrors(formErrors);
+    return formIsValid;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    if (validateForm()) {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_NODE_API}/api/signup`,
+          formData
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
-    <div id="BOOK_A_CALL" class=" min-h-auto mb-0 w-full bg-[#F4F4F4]">
+    <div
+      id="BOOK_A_CALL"
+      className="min-h-auto mb-0 w-full bg-[#F4F4F4] flex flex-col items-center justify-center"
+    >
       <div className="BookCall text-center w-5/6 lg:w-3/5 mx-auto">
         <h2 className="text-5xl lg:text-6xl font-extrabold font-[FatFrank] text-[#3C7278] mt-4">
           book a call
@@ -45,51 +152,69 @@ function BookACall() {
           book a motivational speaking engagement with Eli.
         </p>
       </div>
-      <form class="w-full max-w-lg mt-12">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <p class="text-red-500 text-xs">Form Field Required</p>
+      <form className="w-full max-w-lg mt-12" onSubmit={handleSubmit}>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+            <p className="text-red-500 text-xs h-4">{errors.name}</p>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
+              className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              } rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
+              id="name"
               type="text"
               placeholder="Name"
+              name="name"
+              onChange={handleChange}
             />
           </div>
-          <div class="w-full md:w-1/2 px-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+            <p className="text-red-500 text-xs h-4">{errors.organization}</p>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="organization"
               type="text"
               placeholder="Organization"
+              name="organization"
+              onChange={handleChange}
             />
           </div>
-          <div class="w-full md:w-1/2 px-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+            <p className="text-red-500 text-xs h-4">{errors.title}</p>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="title"
               type="text"
               placeholder="Title/Role"
+              name="title"
+              onChange={handleChange}
             />
           </div>
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <p class="text-red-500 text-xs">Form Field Required</p>
+          <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+            <p className="text-red-500 text-xs h-4">{errors.email}</p>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
-              type="text"
+              className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              } rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white`}
+              id="email"
+              type="email"
               placeholder="Email"
+              name="email"
+              onChange={handleChange}
             />
           </div>
         </div>
-        <p>This cow's udderly excited to work with you!</p>
-        <button
-          id="Question"
-          type="submit"
-          className="bg-gradient-to-r from-greenSlack-100 to-greenSlack-600 hover:bg-gradient-to-r hover:from-greenSlack-200 hover:to-greenSlack-400 block text-white font-mono py-1 px-4 mb-4 mt-2 rounded-lg"
-        >
-          Submit
-        </button>
+        <p className="text-center">
+          This cow's udderly excited to work with you!
+        </p>
+        <div className="flex justify-center">
+          <button
+            id="Question"
+            type="submit"
+            className="bg-gradient-to-r from-greenSlack-100 to-greenSlack-600 hover:bg-gradient-to-r hover:from-greenSlack-200 hover:to-greenSlack-400 block text-white font-mono py-1 px-4 mb-4 mt-2 rounded-lg"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
